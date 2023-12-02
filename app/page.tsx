34eb9d8482b21ef285362b679d775caedc2e9592
SachinG99 <77461836+SachinG99@users.noreply.github.com>
2023-12-02 03:16:41 +0000

    main

--- app/page.tsx
+++ app/page.tsx
@@ -30,7 +30,7 @@ export default function Home() {
       </div>
  
       <div className="flex flex-col gap-2">
-        <h2 className="text-4xl">I am a Project (I am developed by AI)</h2>
+        <h2 className="text-4xl">I am a Project</h2>
         <div className="grid gap-2 grid-cols-1 md:grid-cols-2">
           <div className="p-4 border rounded shadow-md">
             <h3 className="text-3xl text-bold">I am Stockie</h3>
