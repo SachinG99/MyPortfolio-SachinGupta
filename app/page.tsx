150d2a017ea92da48b460d2c1631da9d6a2162c0
SachinG99 <77461836+SachinG99@users.noreply.github.com>
2023-12-02 03:13:45 +0000

    main

--- app/page.tsx
+++ app/page.tsx
@@ -33,52 +33,20 @@ export default function Home() {
         <h2 className="text-4xl">I am a Project (I am developed by AI)</h2>
         <div className="grid gap-2 grid-cols-1 md:grid-cols-2">
           <div className="p-4 border rounded shadow-md">
-            <h3 className="text-3xl text-bold">Project Alpha</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing
-              elit, sed do eiusmod tempor incididunt ut labore et
-              dolore magna aliqua. Ut enim ad minim veniam, quis
-              nostrud exercitation ullamco laboris nisi ut aliquip
-              ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore
-              eu fugiat nulla pariatur. Excepteur sint occaecat
-              cupidatat non proident, sunt in culpa qui officia
-              deserunt mollit anim id est laborum.</p>
+            <h3 className="text-3xl text-bold">I am Stockie</h3>
+            <p>I am a stock analysis tool developed using c++. I help newbies and experts a like invest in stocks more efficiently and effectively. I allow people to compare stocks, stock categories, and future stock projections. I rate stocks and give my daily top 10 stock picks. I am stockie, your personal investment firm.</p>
           </div>
           <div className="p-4 border rounded shadow-md">
-            <h3 className="text-3xl text-bold">Project Beta</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing
-              elit, sed do eiusmod tempor incididunt ut labore et
-              dolore magna aliqua. Ut enim ad minim veniam, quis
-              nostrud exercitation ullamco laboris nisi ut aliquip
-              ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore
-              eu fugiat nulla pariatur. Excepteur sint occaecat
-              cupidatat non proident, sunt in culpa qui officia
-              deserunt mollit anim id est laborum.</p>
+            <h3 className="text-3xl text-bold">I am Teacher</h3>
+            <p>I am a teaching course. I teach computing and the future of technology. Use me to learn about technology and to stay updated on this ever innovating field. Go through my modules at your own pace, and answer the mini quizzes and knowledge games to assert your training into your internal software known as your brain. At the end play the jeopardy game, you deserve it. Finally, you are done, download your certificate and show your soom to be boss!</p>
           </div>
           <div className="p-4 border rounded shadow-md">
-            <h3 className="text-3xl text-bold">Project Gamma</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing
-              elit, sed do eiusmod tempor incididunt ut labore et
-              dolore magna aliqua. Ut enim ad minim veniam, quis
-              nostrud exercitation ullamco laboris nisi ut aliquip
-              ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore
-              eu fugiat nulla pariatur. Excepteur sint occaecat
-              cupidatat non proident, sunt in culpa qui officia
-              deserunt mollit anim id est laborum.</p>
+            <h3 className="text-3xl text-bold">I am Coming Soon</h3>
+            <p>Keep reloading the page to see what comes here!</p>
           </div>
           <div className="p-4 border rounded shadow-md">
-            <h3 className="text-3xl text-bold">Project Delta</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing
-              elit, sed do eiusmod tempor incididunt ut labore et
-              dolore magna aliqua. Ut enim ad minim veniam, quis
-              nostrud exercitation ullamco laboris nisi ut aliquip
-              ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore
-              eu fugiat nulla pariatur. Excepteur sint occaecat
-              cupidatat non proident, sunt in culpa qui officia
-              deserunt mollit anim id est laborum.</p>
+            <h3 className="text-3xl text-bold">I am Coming Very Soon</h3>
+            <p>Keep reloading the page even faster to see what comes here!</p>
           </div>
         </div>
       </div>
